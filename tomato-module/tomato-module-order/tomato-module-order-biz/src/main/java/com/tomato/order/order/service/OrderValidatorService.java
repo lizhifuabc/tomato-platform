42bcea67765b60edec204f60062d45aded4b6f59
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-02 11:40:16 +0800

    order 模块 订单 service

--- tomato-module/tomato-module-order/tomato-module-order-biz/src/main/java/com/tomato/order/order/service/OrderValidatorService.java
+++ tomato-module/tomato-module-order/tomato-module-order-biz/src/main/java/com/tomato/order/order/service/OrderValidatorService.java
@@ -1,10 +1,13 @@
 package com.tomato.order.order.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * 订单验证
  *
  * @author lizhifu
  * @date 2022/12/1
  */
+@Service
 public class OrderValidatorService {
 }
