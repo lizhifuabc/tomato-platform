445c6fd2cf170d03d3326d5244c3fb4b05b95181
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-03 21:30:09 +0800

    系统时钟

---
+++ tomato-framework/tomato-util/src/test/java/com/tomato/util/date/SystemClockTest.java
@@ -0,0 +1,14 @@
+package com.tomato.util.date;
+
+/**
+ * SystemClock
+ *
+ * @author lizhifu
+ * @date 2022/12/3
+ */
+public class SystemClockTest {
+    public static void main(String[] args) {
+        System.out.println(SystemClock.nowDate());
+        System.out.println(SystemClock.now());
+    }
+}
