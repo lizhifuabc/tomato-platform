73fec1e56e0450dd32c922d2c12988c6f1467963
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-06 22:19:55 +0800

    内部监控线程池

--- tomato-framework/tomato-dynamic-thread-stater/src/main/java/com/tomato/dynamic/thread/config/DynamicThreadPoolAutoConfiguration.java
+++ tomato-framework/tomato-dynamic-thread-stater/src/main/java/com/tomato/dynamic/thread/config/DynamicThreadPoolAutoConfiguration.java
@@ -1,7 +1,12 @@
 package com.tomato.dynamic.thread.config;
 
+import com.tomato.dynamic.thread.executor.DynamicThreadPoolExecutor;
+import com.tomato.dynamic.thread.thread.CustomThreadFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 动态线程池自动装配
@@ -12,5 +17,19 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @AutoConfiguration
 @EnableConfigurationProperties(DynamicThreadPoolProperties.class)
 public class DynamicThreadPoolAutoConfiguration {
-
+    /**
+     * 内部监控线程池
+     * @return
+     */
+    @Bean("asyncNotifyTaskExecutor")
+    public DynamicThreadPoolExecutor asyncNotifyTaskExecutor() {
+        CustomThreadFactory customThreadFactory = new CustomThreadFactory("asyncNotifyTaskExecutor");
+        return new DynamicThreadPoolExecutor(
+                2,
+                4,
+                60,
+                new LinkedBlockingQueue(4096),
+                customThreadFactory
+        );
+    }
 }
