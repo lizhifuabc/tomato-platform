c68d5d6a468372ec11453b54b206bc08cc4b3eb5
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-10 21:00:11 +0800

    秒杀 redis lua 脚本

--- tomato-module/tomato-module-goods/tomato-module-goods-biz/src/main/resources/script/seckill.lua
+++ tomato-module/tomato-module-goods/tomato-module-goods-biz/src/main/resources/script/seckill.lua
@@ -4,11 +4,13 @@ if (redis.call('exists', key) == 1) then
     local stock = tonumber(redis.call('get', key));
     -- 获取该商品id的库存，判断库存如果是-1，则直接返回，表示不限制库存
     if (stock == -1) then
+        -- 无库存限制
         return 1;
     end
     -- 如果库存大于0，则扣减库存。
     if (stock > 0) then
         redis.call('incrby', key, -1);
+        -- 返回剩余库存数
         return stock - 1;
     end
     -- 如果库存等于0，是直接返回，表示库存不足。
