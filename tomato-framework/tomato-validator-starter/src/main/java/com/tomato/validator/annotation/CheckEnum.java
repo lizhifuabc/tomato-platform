d50d53765300eac0058b1b74043b50aa18e3d1b1
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-12 21:18:52 +0800

    枚举校验

---
+++ tomato-framework/tomato-validator-starter/src/main/java/com/tomato/validator/annotation/CheckEnum.java
@@ -0,0 +1,50 @@
+package com.tomato.validator.annotation;
+
+
+import com.tomato.domain.type.BaseEnum;
+import com.tomato.validator.validator.EnumValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 枚举值校验
+ *
+ * @author lizhifu
+ * @date 2021/9/14
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EnumValidator.class)// 自定义验证的处理类
+public @interface CheckEnum {
+
+    /**
+     * 默认的错误提示信息
+     *
+     * @return String
+     */
+    String message();
+
+    /**
+     * 枚举类对象 必须实现BaseEnum接口
+     *
+     * @return
+     */
+    Class<? extends BaseEnum> value();
+
+    /**
+     * 是否必须
+     *
+     * @return boolean
+     */
+    boolean required() default false;
+
+    //下面这两个属性必须添加 :不然会报错
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ tomato-framework/tomato-validator-starter/src/main/java/com/tomato/validator/validator/EnumValidator.java
@@ -0,0 +1,73 @@
+package com.tomato.validator.validator;
+
+
+import com.tomato.domain.type.BaseEnum;
+import com.tomato.validator.annotation.CheckEnum;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * 枚举值校验
+ *
+ * @author lizhifu
+ * @date 2021/9/14
+ */
+public class EnumValidator implements ConstraintValidator<CheckEnum, Object> {
+
+    /**
+     * 枚举类实例集合
+     */
+    private List<Object> enumValList;
+
+    /**
+     * 是否必须
+     */
+    private boolean required;
+
+    @Override
+    public void initialize(CheckEnum constraintAnnotation) {
+        // 获取注解传入的枚举类对象
+        required = constraintAnnotation.required();
+        Class<? extends BaseEnum> enumClass = constraintAnnotation.value();
+        enumValList = Stream.of(enumClass.getEnumConstants()).map(BaseEnum::getValue).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
+        // 判断是否必须
+        if (null == value) {
+            return !required;
+        }
+
+        if (value instanceof List) {
+            // 如果为 List 集合数据
+            return this.checkList((List<Object>) value);
+        }
+
+        // 校验是否为合法的枚举值
+        return enumValList.contains(value);
+    }
+
+    /**
+     * 校验集合类型
+     *
+     * @param list
+     * @return
+     */
+    private boolean checkList(List<Object> list) {
+        if (required && list.isEmpty()) {
+            // 必须的情况下 list 不能为空
+            return false;
+        }
+        // 校验是否重复
+        long count = list.stream().distinct().count();
+        if (count != list.size()) {
+            return false;
+        }
+        return enumValList.containsAll(list);
+    }
+}
