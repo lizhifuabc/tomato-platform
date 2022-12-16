2ed1c256de5c4ce2c8e682e638d3d8039e4733b0
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-16 11:55:08 +0800

    xss 校验注解实现

---
+++ tomato-framework/tomato-validator-starter/src/main/java/com/tomato/validator/annotation/Xss.java
@@ -0,0 +1,30 @@
+package com.tomato.validator.annotation;
+
+import com.tomato.validator.validator.XssValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * xss 校验注解
+ * 
+ * @author lizhfiu
+ * @since 2022年12月16日
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.PARAMETER })
+@Constraint(validatedBy = { XssValidator.class })
+public @interface Xss
+{
+    String message()
+
+    default "不允许任何脚本运行";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ tomato-framework/tomato-validator-starter/src/main/java/com/tomato/validator/validator/XssValidator.java
@@ -0,0 +1,37 @@
+package com.tomato.validator.validator;
+
+import com.tomato.validator.annotation.Xss;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * xss 校验注解实现
+ * 
+ * @author lizhifu
+ */
+public class XssValidator implements ConstraintValidator<Xss, String>
+{
+    /**
+     * HTML
+     */
+    private static final String HTML_PATTERN = "<(\\S*?)[^>]*>.*?|<.*? />";
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext)
+    {
+        if (value == null || value.length() == 0 ) {
+            return true;
+        }
+        return !containsHtml(value);
+    }
+
+    public static boolean containsHtml(String value)
+    {
+        Pattern pattern = Pattern.compile(HTML_PATTERN);
+        Matcher matcher = pattern.matcher(value);
+        return matcher.matches();
+    }
+}
