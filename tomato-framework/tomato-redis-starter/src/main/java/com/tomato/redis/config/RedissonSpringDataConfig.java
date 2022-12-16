b2cca7f2b402c4844c9c114c1e0764ac86800990
拔土豆的程序员 <lizhifuabc@163.com>
2022-12-16 11:28:52 +0800

    Redisson

---
+++ tomato-framework/tomato-redis-starter/src/main/java/com/tomato/redis/config/RedissonSpringDataConfig.java
@@ -0,0 +1,32 @@
+package com.tomato.redis.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+/**
+ * Register RedissonConnectionFactory in Spring context
+ * 在Spring上下文中注册 RedissonConnectionFactory
+ * @author lizhifu
+ * @since 2022/12/16
+ */
+@AutoConfiguration
+public class RedissonSpringDataConfig {
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
+        return new RedissonConnectionFactory(redisson);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redisson(@Value("classpath:/redisson.yaml") Resource configFile) throws IOException {
+        Config config = Config.fromYAML(configFile.getInputStream());
+        return Redisson.create(config);
+    }
+}
