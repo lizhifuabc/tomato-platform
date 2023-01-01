ff3157a894a0380e0a38494f198c0b4922e36660
拔土豆的程序员 <lizhifuabc@163.com>
2023-01-01 15:25:35 +0800

    task 商户编号生成

--- doc/sql/tomato-module-merchant.sql
+++ doc/sql/tomato-module-merchant.sql
@@ -1,3 +1,16 @@
+# schema
+drop schema if exists `tomato_merchant`;
+create schema `tomato_merchant`;
+use `tomato_merchant`;
 ## JPA 自动创建表
-INSERT INTO t_merchant_incr
-(id, create_time, update_time, version, merchant_no) VALUES (1, '2022-12-15 23:05:48', '2022-12-15 23:05:50', 0, 1000001);
+-- auto-generated definition
+drop table if exists `t_merchant_incr`;
+create table `t_merchant_incr`
+(
+    `id`          bigint auto_increment primary key,
+    `merchant_no` bigint        null,
+    `version` int default 0 not null comment '乐观锁',
+    `update_time` datetime not null default current_timestamp on update current_timestamp comment '更新时间',
+    `create_time` datetime not null default current_timestamp comment '创建时间'
+);
+INSERT INTO t_merchant_incr(`merchant_no`) VALUES (1000);
