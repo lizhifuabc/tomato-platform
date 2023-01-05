0c9e883e5589c7e7aeb0052899501f573e7d4db1
拔土豆的程序员 <lizhifuabc@163.com>
2023-01-06 00:07:34 +0800

    task 通知系统创建通知记录

--- tomato-module/tomato-module-notice/tomato-module-notice-base/src/main/java/com/tomato/notice/domain/req/NoticeCreateReq.java
+++ tomato-module/tomato-module-notice/tomato-module-notice-base/src/main/java/com/tomato/notice/domain/req/NoticeCreateReq.java
@@ -1,6 +1,8 @@
 package com.tomato.notice.domain.req;
 
 import com.tomato.notice.domain.entity.NoticeRecordEntity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.util.Map;
@@ -16,30 +18,36 @@ public class NoticeCreateReq {
     /**
      * 订单号
      */
+    @NotBlank
     private String orderNo;
 
     /**
      * 商户编号
      */
+    @NotBlank
     private String merchantNo;
 
     /**
      * 商户订单号
      */
+    @NotBlank
     private String merchantOrderNo;
 
     /**
      * 系统编号(对接多个系统使用)
      */
+    @NotBlank
     private String appNo;
 
     /**
      * 通知地址
      */
+    @NotBlank
     private String noticeUrl;
     /**
      * 通知参数
      */
+    @NotEmpty
     private Map<String,String> noticeParam;
 
 }
