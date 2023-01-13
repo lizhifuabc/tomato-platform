7678d6d4d218732bceb6ffa28cf67fe4d98ae0d0
拔土豆的程序员 <lizhifuabc@163.com>
2023-01-13 12:01:10 +0800

    task 账户系统：定时测试

---
+++ tomato-module/tomato-module-account/tomato-module-account-biz/src/test/java/com/tomato/account/timer/TimerTest.java
@@ -0,0 +1,30 @@
+package com.tomato.account.timer;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * TimerTest
+ *
+ * @author lizhifu
+ * @since 2023/1/13
+ */
+@SpringBootTest
+public class TimerTest {
+    @Resource
+    AccountSettleTimer accountSettleTimer;
+    @Resource
+    AccountOutReserveBalanceTimer accountOutReserveBalanceTimer;
+    @Resource
+    AccountTradAsyncTimer accountTradAsyncTimer;
+
+    @Test
+    public void timer(){
+        System.out.println("执行所有定时start");
+        accountSettleTimer.run();
+        accountTradAsyncTimer.run();
+        accountOutReserveBalanceTimer.run();
+        System.out.println("执行所有定时end");
+    }
+}
