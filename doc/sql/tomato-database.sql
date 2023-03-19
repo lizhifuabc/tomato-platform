96f7fb3c58a6ebc3b79c15666d58195335935e6f
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-19 16:39:33 +0800

    商品系统

--- doc/sql/tomato-database.sql
+++ doc/sql/tomato-database.sql
@@ -6,3 +6,5 @@
 -- 秒杀数据库
 -- ----------------------------
 CREATE DATABASE IF NOT EXISTS `tomato-seckill` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE IF NOT EXISTS `tomato-goods` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
