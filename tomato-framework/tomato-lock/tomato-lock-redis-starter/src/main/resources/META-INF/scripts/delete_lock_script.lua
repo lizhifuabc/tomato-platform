fe19eb80967a8c93b6ed49b7907b221d90fdde57
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-19 16:33:50 +0800

    lua 脚本添加描述

--- tomato-framework/tomato-lock/tomato-lock-redis-starter/src/main/resources/META-INF/scripts/delete_lock_script.lua
+++ tomato-framework/tomato-lock/tomato-lock-redis-starter/src/main/resources/META-INF/scripts/delete_lock_script.lua
@@ -1,4 +1,5 @@
 -- 判断数据 lockValue 是否相同
+redis.log(redis.LOG_WARNING, 'try to delete lock: ' .. KEYS[1] .. ', owner: ' .. ARGV[1])
 if redis.call('get', KEYS[1]) == ARGV[1]
 then
     return redis.call('del', KEYS[1])
