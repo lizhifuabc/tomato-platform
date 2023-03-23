89bb071739df7dea575c13ad51de7058be2a2891
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-23 22:32:45 +0800

    基础mapper

---
+++ tomato-framework/tomato-mybatis-starter/src/main/java/com/tomato/mybatis/mapper/BaseMapper.java
@@ -0,0 +1,14 @@
+package com.tomato.mybatis.mapper;
+
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 基础mapper
+ *
+ * @author lizhifu
+ * @since 2023/3/23
+ */
+public interface BaseMapper<T> {
+
+}
