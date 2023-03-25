67c6e06122d6b637f50b4174202c56b61ae77d9e
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-25 14:37:37 +0800

    redis 基础依赖包

--- tomato-framework/tomato-redis-starter/src/main/java/com/tomato/redis/config/RedisAutoConfigure.java
+++ tomato-framework/tomato-redis-starter/src/main/java/com/tomato/redis/config/RedisAutoConfigure.java
@@ -27,33 +27,6 @@ import java.util.List;
 @AutoConfiguration
 public class RedisAutoConfigure {
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory factory) {
-        StringRedisTemplate template = new StringRedisTemplate();
-        template.setConnectionFactory(factory);
-        return template;
-    }
-    /**
-     * 创建 RedisTemplate Bean，使用 JSON 序列化方式
-     */
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-        // 创建 RedisTemplate 对象
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        // 设置 RedisConnection 工厂
-        template.setConnectionFactory(factory);
-        // 使用 String 序列化方式，序列化 KEY 。
-        template.setKeySerializer(RedisSerializer.string());
-        template.setHashKeySerializer(RedisSerializer.string());
-        // 使用 JSON 序列化方式（库是 Jackson ），序列化 VALUE 。
-        // 区别于Jackson2JsonRedisSerializer，GenericJackson2JsonRedisSerializer,会额外存储序列化对象的包命和类名
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(mapper);
-        template.setValueSerializer(serializer);
-        template.setHashValueSerializer(serializer);
-        return template;
-    }
-    @Bean
     public RedisScript redisRequestRateLimiterScript() {
         DefaultRedisScript redisScript = new DefaultRedisScript<>();
         redisScript.setScriptSource(
