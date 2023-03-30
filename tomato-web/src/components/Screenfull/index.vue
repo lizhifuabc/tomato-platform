caf7ea83ec91162a013d735cf7ce107f12f792de
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-30 16:34:27 +0800

    web 项目初始化

---
+++ tomato-web/src/components/Screenfull/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { isFullscreen, toggle } = useFullscreen();
+</script>
+
+<template>
+  <div>
+    <svg-icon
+      :icon-class="isFullscreen ? 'exit-fullscreen' : 'fullscreen'"
+      @click="toggle"
+    />
+  </div>
+</template>
