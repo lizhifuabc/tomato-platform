43c586a811234e22eaf2e91c19112e5e5b17df4a
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-31 16:23:13 +0800

    web 项目初始化

--- tomato-gateway/src/main/java/com/tomato/gateway/GatewayApplication.java
+++ tomato-gateway/src/main/java/com/tomato/gateway/GatewayApplication.java
@@ -3,8 +3,6 @@ package com.tomato.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 /**
  * 网关启动程序
  *
@@ -13,7 +11,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @Slf4j
 @SpringBootApplication
-@EnableDiscoveryClient // 开启服务注册发现
 public class GatewayApplication {
     public static void main(String[] args) {
         log.info("Begin to start Spring Boot Application");
--- tomato-web/src/views/nested/level1/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="padding: 30px">
-    <el-alert :closable="false" title="菜单一级">
-      <router-view />
-    </el-alert>
-  </div>
-</template>
