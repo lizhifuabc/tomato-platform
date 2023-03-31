6b8719cd4a91a50284724eb756e7cace5f57aff0
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-31 20:48:18 +0800

    基于开源项目初始化

--- tomato-web/src/api/config/servicePort.ts
+++
@@ -1,3 +0,0 @@
-// * 后端微服务端口名
-export const PORT1 = "/geeker";
-export const PORT2 = "/hooks";
--- tomato-web/src/views/system/menuMange/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="card content-box">
-		<span class="text"> 菜单管理（待完善） 🍓🍇🍈🍉</span>
-	</div>
-</template>
-
-<script setup lang="ts" name="menuMange"></script>
