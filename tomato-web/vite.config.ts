81635744ba3c0b96a8ee8d603aecb09aa5833b01
拔土豆的程序员 <lizhifuabc@163.com>
2023-03-31 21:35:40 +0800

    web 项目初始化

--- tomato-web/vite.config.ts
+++ tomato-web/vite.config.ts
@@ -5,12 +5,6 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   return {
-    base: "./",
-    resolve: {
-      alias: {
-        "@": resolve(__dirname, "./src"),
-      }
-    },
     plugins: [vue()],
   }
 })
