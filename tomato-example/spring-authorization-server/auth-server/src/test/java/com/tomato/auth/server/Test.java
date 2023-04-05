5853d582da3555e706f8757d702da517609df362
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-05 23:22:15 +0800

    spring-authorization-server 代码示例 😭

--- tomato-example/spring-authorization-server/auth-server/src/test/java/com/tomato/auth/server/Test.java
+++ tomato-example/spring-authorization-server/auth-server/src/test/java/com/tomato/auth/server/Test.java
@@ -4,12 +4,18 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  * TODO
+ * 1. REDIRECT_URI,使用错误的地址
+ * 2. scope 为demo,数据库为demo
+ * 3. scope 为openid,数据库为demo
+ * 4. scope 为openid,数据库为openid
+ * TODO 1 lb方式调用授权服务，需要配置负载均衡 2 scope权限获取不到
  *
  * @author lizhifu
  * @since 2023/4/5
  */
 public class Test {
-    private static final String REDIRECT_URI = "https://www.baidu.com";
+
+    private static final String REDIRECT_URI = "http://127.0.0.1:9001";
 
     private static final String AUTHORIZATION_REQUEST = UriComponentsBuilder
             .fromPath("/oauth2/authorize")
