ca9785377a5ef4cedbfe9e7a14f12ba6eb834cb7
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-06 18:10:51 +0800

    spring-authorization-server 代码示例

--- tomato-example/spring-authorization-server/auth-server/src/main/java/com/tomato/auth/server/AuthServerApplication.java
+++ tomato-example/spring-authorization-server/auth-server/src/main/java/com/tomato/auth/server/AuthServerApplication.java
@@ -2,7 +2,6 @@ package com.tomato.auth.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 授权服务中心启动类
@@ -11,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @since 2023/4/2
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AuthServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
