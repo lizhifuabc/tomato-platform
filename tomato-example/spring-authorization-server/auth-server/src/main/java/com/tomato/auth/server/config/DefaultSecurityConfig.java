6b72c0b0880a1e71e91d7d9a7e8c6d1c5e1f13a6
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-06 20:39:58 +0800

    spring-authorization-server 代码示例

--- tomato-example/spring-authorization-server/auth-server/src/main/java/com/tomato/auth/server/config/DefaultSecurityConfig.java
+++ tomato-example/spring-authorization-server/auth-server/src/main/java/com/tomato/auth/server/config/DefaultSecurityConfig.java
@@ -8,10 +8,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
