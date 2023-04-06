cbd2c21b550686dc90bc11d71ea6eb702826a9e8
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-06 21:41:01 +0800

    spring-authorization-server 资源服务器

---
+++ tomato-example/spring-authorization-server/auth-resource/src/main/java/com/tomato/resource/ResourceApplication.java
@@ -0,0 +1,17 @@
+package com.tomato.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 资源服务器
+ *
+ * @author lizhifu
+ * @since 2023/4/2
+ */
+@SpringBootApplication
+public class ResourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+}
---
+++ tomato-example/spring-authorization-server/auth-resource/src/main/java/com/tomato/resource/controller/ResourceController.java
@@ -0,0 +1,20 @@
+package com.tomato.resource.controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 资源服务器
+ *
+ * @author lizhifu
+ * @since 2023/4/6
+ */
+@RestController
+public class ResourceController {
+    @GetMapping("/hello")
+    public HttpEntity<String> hello() {
+        return ResponseEntity.ok("auth-resource -> hello");
+    }
+}
