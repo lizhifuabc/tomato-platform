3f96b928307c16ede6e9a4b0fe54bea7ab841605
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-19 17:43:32 +0800

    计算账户风险预存期外余额

--- tomato-module/tomato-module-account/tomato-module-account-biz/src/main/java/com/tomato/account/timer/AccountOutReserveBalanceTimer.java
+++ tomato-module/tomato-module-account/tomato-module-account-biz/src/main/java/com/tomato/account/timer/AccountOutReserveBalanceTimer.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
