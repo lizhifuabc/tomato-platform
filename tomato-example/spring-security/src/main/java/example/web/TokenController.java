9d2ad4d0c93ce232e2bc2a2a4bedffc030b03d76
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-22 23:26:13 +0800

    demo

---
+++ tomato-example/spring-security/src/main/java/example/web/TokenController.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+/**
+ * A controller for the token resource.
+ *
+ * @author Josh Cummings
+ */
+@RestController
+public class TokenController {
+
+	@Autowired
+	JwtEncoder encoder;
+
+	@PostMapping("/token")
+	public String token(Authentication authentication) {
+		Instant now = Instant.now();
+		long expiry = 36000L;
+		// @formatter:off
+		String scope = authentication.getAuthorities().stream()
+				.map(GrantedAuthority::getAuthority)
+				.collect(Collectors.joining(" "));
+		JwtClaimsSet claims = JwtClaimsSet.builder()
+				.issuer("self")
+				.issuedAt(now)
+				.expiresAt(now.plusSeconds(expiry))
+				.subject(authentication.getName())
+				.claim("scope", scope)
+				.build();
+		// @formatter:on
+		return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+	}
+
+}
