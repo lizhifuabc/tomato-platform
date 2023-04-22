be2a4a68f2625cf52cae3274ea791535de803f9f
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-22 21:55:54 +0800

    代码优化

--- tomato-module/tomato-module-sys/tomato-module-sys-infrastructure/src/main/java/com/tomato/sys/infrastructure/security/config/SecurityConfig.java
+++ tomato-module/tomato-module-sys/tomato-module-sys-infrastructure/src/main/java/com/tomato/sys/infrastructure/security/config/SecurityConfig.java
@@ -1,14 +1,13 @@
 package com.tomato.sys.infrastructure.security.config;
 
 import com.tomato.security.config.AbstractSecurityConfig;
-import com.tomato.sys.infrastructure.repository.SysUserRepository;
-import com.tomato.sys.infrastructure.security.LoginService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.function.BiFunction;
 
@@ -23,9 +22,10 @@ import java.util.function.BiFunction;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends AbstractSecurityConfig {
     @Resource
-    private LoginService loginService;
+    private UserDetailsService userDetailsService;
     @Override
     protected BiFunction<String, HttpServletRequest, UserDetails> userFunction() {
-        return (token, request) -> loginService.getLoginUserDetail(token, request);
+        // return (token, request) -> userDetailsService.loadUserByUsername(token,request);
+        return (token, request) -> userDetailsService.loadUserByUsername(token);
     }
 }
