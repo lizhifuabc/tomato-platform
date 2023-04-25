13f34868f8dc3a4c9c3ba5f2c320c3bd765c6cfc
拔土豆的程序员 <lizhifuabc@163.com>
2023-04-25 12:45:35 +0800

    feature：根据规则编码查询

---
+++ tomato-module/tomato-module-notice/tomato-module-notice-biz/src/main/java/com/tomato/notice/manager/NoticeRuleManager.java
@@ -0,0 +1,26 @@
+package com.tomato.notice.manager;
+
+import com.tomato.notice.domain.entity.NoticeRuleEntity;
+import com.tomato.notice.mapper.NoticeRuleMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * 通知规则
+ *
+ * @author lizhifu
+ * @since 2023/4/25
+ */
+@Service
+public class NoticeRuleManager {
+    private final NoticeRuleMapper noticeRuleMapper;
+
+    public NoticeRuleManager(NoticeRuleMapper noticeRuleMapper) {
+        this.noticeRuleMapper = noticeRuleMapper;
+    }
+
+    public Optional<NoticeRuleEntity> selectByRuleCode(String ruleCode) {
+        return Optional.ofNullable(noticeRuleMapper.selectByRuleCode(ruleCode));
+    }
+}
