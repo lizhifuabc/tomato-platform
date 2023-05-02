2ac6ce76f5104f4eb01cae11fd8413f782dcb28b
拔土豆的程序员 <lizhifuabc@163.com>
2023-05-02 21:18:46 +0800

    feature：tomato-cloud-feign

---
+++ tomato-framework/tomato-cloud/tomato-cloud-feign/src/main/java/com/tomato/cloud/feign/AutoConfiguration.java
@@ -0,0 +1,21 @@
+package com.tomato.cloud.feign;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自动配置
+ *
+ * @author lizhifu
+ * @since 2023/5/2
+ */
+@Configuration(proxyBeanMethods = false)
+public class AutoConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(AutoConfiguration.class);
+    @PostConstruct
+    public void postConstruct() {
+        log.info("spring cloud feign 自动配置");
+    }
+}
