0da9db1558fd234ddcfa93bf84812533dead70b5
拔土豆的程序员 <lizhifuabc@163.com>
2023-05-16 15:35:09 +0800

    feature：prometheus 监控配置

---
+++ tomato-framework/tomato-monitor/tomato-monitor-prometheus/README.md
@@ -0,0 +1,9 @@
+https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#actuator.metrics.export.prometheus
+
+
+http://127.0.0.1:9996/actuator/prometheus
+
+
+# 启用 prometheus和health监控
+management.endpoints.web.exposure.include=prometheus,health
+management.prometheus.metrics.export.enabled=true
---
+++ tomato-framework/tomato-monitor/tomato-monitor-prometheus/src/main/java/com/tomato/monitor/prometheus/PrometheusAutoConfiguration.java
@@ -0,0 +1,30 @@
+package com.tomato.monitor.prometheus;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 自动配置
+ *
+ * @author lizhifu
+ * @since 2023/5/2
+ */
+@AutoConfiguration
+public class PrometheusAutoConfiguration {
+    private static final Logger log = LoggerFactory.getLogger(PrometheusAutoConfiguration.class);
+    @PostConstruct
+    public void postConstruct() {
+        log.info("tomato-monitor-prometheus 自动配置");
+    }
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        // # 为指标设置一个Tag，这里设置为应用名，Tag是Prometheus提供的一种能力，从而实现更加灵活的筛选
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+}
