cf141b2316d04bf7375339527918465f0e31c3d5
拔土豆的程序员 <lizhifuabc@163.com>
2023-05-23 20:42:02 +0800

    feat: redis 常量

---
+++ tomato-module/tomato-module-pay/tomato-module-pay-domain/src/main/java/com/tomato/pay/domain/constants/RedisConstant.java
@@ -0,0 +1,12 @@
+package com.tomato.pay.domain.constants;
+
+/**
+ * Redis 常量
+ *
+ * @author lizhifu
+ * @since 2023/5/23
+ */
+public interface RedisConstant {
+    String key = "routeAlarm:";
+    String PAY_TYPE = "pay_type";
+}
