9595e32e622c1c6726d9e267462881c77776c3f4
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-01 21:59:33 +0800

    [新增] 异步任务执行

---
+++ tomato-framework/tomato-common/src/main/java/com/tomato/common/async/Async.java
@@ -0,0 +1,25 @@
+package com.tomato.common.async;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 异步任务执行
+ *
+ * @author lizhifu
+ * @since 2023/6/1
+ */
+public class Async {
+    /**
+     * 公共线程池
+     */
+    private static final ThreadPoolExecutor COMMON_POOL = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+    public static boolean exe(){
+        CompletableFuture.runAsync(() -> {
+            System.out.println("异步任务");
+        },COMMON_POOL);
+        return true;
+    }
+
+}
---
+++ tomato-framework/tomato-security-starter/src/main/java/com/tomato/security/token/TokenRepository.java
@@ -0,0 +1,11 @@
+package com.tomato.security.token;
+
+/**
+ * token 存储
+ *
+ * @author lizhifu
+ * @since 2023/5/31
+ */
+public interface TokenRepository {
+
+}
