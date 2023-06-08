a99a4e6b4b643d538a4ba20be52ec876a5e7eacf
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-08 23:02:35 +0800

    [重构] 权限设计

--- tomato-module/tomato-module-sys/tomato-module-sys-infrastructure/src/main/java/com/tomato/sys/infrastructure/security/config/ApplicationConfig.java
+++ tomato-module/tomato-module-sys/tomato-module-sys-infrastructure/src/main/java/com/tomato/sys/infrastructure/security/config/ApplicationConfig.java
@@ -1,6 +1,15 @@
 package com.tomato.sys.infrastructure.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 应用配置
@@ -10,4 +19,32 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ApplicationConfig {
+    private final UserDetailsService userDetailsService;
+
+    public ApplicationConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // TODO  username -> repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("用户不存在"));
+
+        return userDetailsService;
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 }
