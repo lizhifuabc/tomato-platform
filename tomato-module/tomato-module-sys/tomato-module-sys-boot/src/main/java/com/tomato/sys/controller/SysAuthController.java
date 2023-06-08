2e932c95f38d1b71012d1e03948ce7493a93d404
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-08 22:23:43 +0800

    [重构] 权限设计

---
+++ tomato-module/tomato-module-sys/tomato-module-sys-boot/src/main/java/com/tomato/sys/controller/SysAuthController.java
@@ -0,0 +1,13 @@
+package com.tomato.sys.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 授权相关
+ *
+ * @author lizhifu
+ * @since 2023/6/8
+ */
+@RestController
+public class SysAuthController {
+}
