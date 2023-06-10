5509bd9a1de10a3b4fb26879fc412ca4fc389126
拔土豆的程序员 <lizhifuabc@163.com>
2023-06-10 15:10:38 +0800

    [新增] spring security 用户退出逻辑

--- tomato-module/tomato-module-sys/tomato-module-sys-infrastructure/src/main/java/com/tomato/sys/infrastructure/security/config/LogoutService.java
+++ tomato-module/tomato-module-sys/tomato-module-sys-infrastructure/src/main/java/com/tomato/sys/infrastructure/security/config/LogoutService.java
@@ -1,9 +1,16 @@
 package com.tomato.sys.infrastructure.security.config;
 
 import com.tomato.sys.domain.constants.RequestHeaderConstant;
+import com.tomato.sys.infrastructure.repository.SysTokenRepository;
+import com.tomato.sys.infrastructure.repository.SysUserRepository;
+import com.tomato.sys.infrastructure.security.user.SecurityUserDetails;
+import com.tomato.sys.infrastructure.security.user.UserDetailsServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +21,30 @@ import org.springframework.stereotype.Service;
  * @since 2023/6/8
  */
 @Service
+@Slf4j
 public class LogoutService implements LogoutHandler {
+    private final SysTokenRepository tokenRepository;
+    private final JwtService jwtService;
+    private final SysUserRepository sysUserRepository;
+    public LogoutService(SysTokenRepository tokenRepository, JwtService jwtService, SysUserRepository sysUserRepository) {
+        this.tokenRepository = tokenRepository;
+        this.jwtService = jwtService;
+        this.sysUserRepository = sysUserRepository;
+    }
+
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         final String authHeader = request.getHeader(RequestHeaderConstant.TOKEN);
         if (authHeader == null ||!authHeader.startsWith(RequestHeaderConstant.AUTHORIZATION_BEARER)) {
             return;
         }
-        final String jwt;
+        String username = jwtService.extractUsername(authHeader.substring(7));
+        log.info("用户退出:{}",username);
+        sysUserRepository.findByUsername(username).flatMap(tokenRepository::findBySysUser).ifPresent(t -> {
+            t.setExpired(true);
+            t.setRevoked(true);
+            tokenRepository.save(t);
+        });
+        SecurityContextHolder.clearContext();
     }
 }
