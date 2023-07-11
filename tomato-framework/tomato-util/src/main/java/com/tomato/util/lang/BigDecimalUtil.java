2382d7dab1f42debc599f1030ec4bde5b6d46dda
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-11 12:17:32 +0800

    [修复] double 使用 BigDecimal.valueOf 精度问题

--- tomato-framework/tomato-util/src/main/java/com/tomato/util/lang/BigDecimalUtil.java
+++ tomato-framework/tomato-util/src/main/java/com/tomato/util/lang/BigDecimalUtil.java
@@ -7,7 +7,7 @@ import java.math.RoundingMode;
  * BigDecimal
  *
  * @author lizhifu
- * @date 2022/6/22
+ * @since  2022/6/22
  */
 public class BigDecimalUtil {
     /**
@@ -28,8 +28,8 @@ public class BigDecimalUtil {
      */
 
     public static double add(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.add(b2).doubleValue();
     }
 
@@ -72,8 +72,8 @@ public class BigDecimalUtil {
      * @return 两个参数的差
      */
     public static double sub(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.subtract(b2).doubleValue();
     }
 
@@ -116,8 +116,8 @@ public class BigDecimalUtil {
      * @return 两个参数的积
      */
     public static double mul(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.multiply(b2).doubleValue();
     }
 
@@ -143,8 +143,8 @@ public class BigDecimalUtil {
      * @return 两个参数的积
      */
     public static double mul(double v1, double v2, int scale) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         return round(b1.multiply(b2).doubleValue(), scale);
     }
 
@@ -179,8 +179,8 @@ public class BigDecimalUtil {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
     }
 
@@ -213,7 +213,7 @@ public class BigDecimalUtil {
         if (scale < 0) {
             throw new IllegalArgumentException("The scale must be a positive integer or zero");
         }
-        BigDecimal b = new BigDecimal(Double.toString(v));
+        BigDecimal b = BigDecimal.valueOf(v);
         return b.setScale(scale, RoundingMode.HALF_UP).doubleValue();
     }
 
@@ -266,7 +266,62 @@ public class BigDecimalUtil {
         }
         return v1.remainder(v2).setScale(scale, RoundingMode.HALF_UP);
     }
+    /**
+     * 提供精确的类型转换(Float)
+     *
+     * @param v 需要被转换的数字
+     * @return 返回转换结果
+     */
+    public static float convertToFloat(double v) {
+        BigDecimal b = BigDecimal.valueOf(v);
+        return b.floatValue();
+    }
 
+    /**
+     * 提供精确的类型转换(Int)不进行四舍五入
+     *
+     * @param v 需要被转换的数字
+     * @return 返回转换结果
+     */
+    public static int convertsToInt(double v) {
+        BigDecimal b = BigDecimal.valueOf(v);
+        return b.intValue();
+    }
+    /**
+     * 提供精确的类型转换(Long)
+     *
+     * @param v 需要被转换的数字
+     * @return 返回转换结果
+     */
+    public static long convertsToLong(double v) {
+        BigDecimal b = BigDecimal.valueOf(v);
+        return b.longValue();
+    }
+    /**
+     * 返回两个数中大的一个值
+     *
+     * @param v1 需要被对比的第一个数
+     * @param v2 需要被对比的第二个数
+     * @return 返回两个数中大的一个值
+     */
+    public static double returnMax(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.max(b2).doubleValue();
+    }
+
+    /**
+     * 返回两个数中小的一个值
+     *
+     * @param v1 需要被对比的第一个数
+     * @param v2 需要被对比的第二个数
+     * @return 返回两个数中小的一个值
+     */
+    public static double returnMin(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.min(b2).doubleValue();
+    }
     /**
      * 比较大小
      *
@@ -277,6 +332,22 @@ public class BigDecimalUtil {
     public static boolean compare(String v1, String v2) {
         BigDecimal b1 = new BigDecimal(v1);
         BigDecimal b2 = new BigDecimal(v2);
+        int bj = b1.compareTo(b2);
+        boolean res;
+        res = bj > 0;
+        return res;
+    }
+    /**
+     * 比较大小
+     *
+     * @param v1 被比较数
+     * @param v2 比较数
+     * @return 如果v1 大于v2 则 返回true 否则false
+     */
+    public static boolean compare(double v1, double v2) {
+        // 精准运算不要使用 new BigDecimal(double d)的方式，而是使用BigDecimal.valueOf(double d)
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
         int bj = b1.compareTo(b2);
         boolean res;
         res = bj > 0;
