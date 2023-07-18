48d86e1d95d155637ec2c92ad92596bf31f152de
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-18 12:27:45 +0800

    [新增] 基础web：全局返回增加 traceId、账户系统|增加日志traceId，spring doc 全局依赖管理

--- tomato-framework/tomato-web-starter/tomato-web-core/src/main/java/com/tomato/web/core/handler/ResponseAdviceHandler.java
+++ tomato-framework/tomato-web-starter/tomato-web-core/src/main/java/com/tomato/web/core/handler/ResponseAdviceHandler.java
@@ -1,6 +1,10 @@
 package com.tomato.web.core.handler;
 
 import com.tomato.common.resp.Resp;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import jakarta.annotation.Resource;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -9,21 +13,25 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import java.util.Objects;
+
 import static com.tomato.common.constants.CommonRespCode.INTERNAL_SERVER_ERROR;
 
 /**
  * Controller 返回的内容在 HttpMessageConverter 进行类型转换之前拦截，进行相应的处理操作后，再将结果返回给客户端
  *
  * @author lizhifu
- * @date 2022/12/12
+ * @since  2022/12/12
  */
 @ControllerAdvice
 public class ResponseAdviceHandler implements ResponseBodyAdvice<Object> {
+    @Resource
+    private Tracer tracer;
     /**
      * 判断是否要交给 beforeBodyWrite 方法执行，ture：需要；false：不需要
-     * @param returnType
-     * @param converterType
-     * @return
+     * @param returnType 返回值类型
+     * @param converterType 转换类型
+     * @return 是否需要执行
      */
     @Override
     public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
@@ -36,18 +44,26 @@ public class ResponseAdviceHandler implements ResponseBodyAdvice<Object> {
 
     /**
      * 对 response 进行具体的处理
-     * @param body
-     * @param returnType
-     * @param selectedContentType
-     * @param selectedConverterType
-     * @param request
-     * @param response
-     * @return
+     * @param body 返回值
+     * @param returnType 返回值类型
+     * @param selectedContentType 返回类型
+     * @param selectedConverterType 转换类型
+     * @param request 请求
+     * @param response 响应
+     * @return 处理后的结果
      */
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+    public Object beforeBodyWrite(Object body, @NotNull MethodParameter returnType, @NotNull MediaType selectedContentType, @NotNull Class<? extends HttpMessageConverter<?>> selectedConverterType, @NotNull ServerHttpRequest request, @NotNull ServerHttpResponse response) {
         if(body instanceof Throwable){
             return Resp.buildFailure(INTERNAL_SERVER_ERROR.code(),INTERNAL_SERVER_ERROR.msg());
+        }
+        if (body instanceof Resp) {
+            Resp resp = (Resp) body;
+            Span span = tracer.currentSpan();
+            if (Objects.nonNull(span)) {
+                resp.setTraceId(span.context().traceId());
+            }
+            return resp;
         }
         return body;
     }
