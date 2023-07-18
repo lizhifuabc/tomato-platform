968a5a025a280d6d8e00ee9b63ff9ec5d407f203
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-18 12:04:41 +0800

    [重构] 异常体系

---
+++ tomato-module/tomato-module-account/tomato-module-account-base/src/main/java/com/tomato/account/exception/AccountException.java
@@ -0,0 +1,12 @@
+package com.tomato.account.exception;
+
+import com.tomato.common.exception.BusinessException;
+
+/**
+ * 账户系统异常
+ *
+ * @author lizhifu
+ * @since 2023/7/18
+ */
+public class AccountException extends BusinessException {
+}
