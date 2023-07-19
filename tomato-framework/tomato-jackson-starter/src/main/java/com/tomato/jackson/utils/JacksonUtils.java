4306088f305b4a4223d0a0dc87e6a580ecd4a6a1
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-19 17:27:15 +0800

    [新增] Jackson 工具类 增加描述信息

--- tomato-framework/tomato-jackson-starter/src/main/java/com/tomato/jackson/utils/JacksonUtils.java
+++ tomato-framework/tomato-jackson-starter/src/main/java/com/tomato/jackson/utils/JacksonUtils.java
@@ -11,20 +11,17 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import jakarta.annotation.PostConstruct;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.function.Function;
 
 /**
- * json util 区别 阿里的fastjson
+ * Jackson 工具类
+ *
  * @author lizhifu
  */
-public class JacksonUtils {
+public record JacksonUtils(ObjectMapper objectMapper) {
     private static ObjectMapper OBJECT_MAPPER;
-    private final ObjectMapper objectMapper;
-
-    public JacksonUtils(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     @PostConstruct
     public void init() {
@@ -35,6 +32,7 @@ public class JacksonUtils {
         OBJECT_MAPPER = this.objectMapper;
     }
 
+    @Override
     public static ObjectMapper objectMapper() {
         return OBJECT_MAPPER;
     }
@@ -43,6 +41,13 @@ public class JacksonUtils {
         return objectMapper().registerModules(module);
     }
 
+    /**
+     * JavaBean转换为json字符串
+     *
+     * @param domain JavaBean
+     * @param <T>    泛型
+     * @return json字符串
+     */
     public static <T> String toJson(T domain) {
         try {
             return objectMapper().writeValueAsString(domain);
@@ -55,6 +60,14 @@ public class JacksonUtils {
         return objectMapper().getTypeFactory();
     }
 
+    /**
+     * json字符串转换为JavaBean
+     *
+     * @param content   json字符串
+     * @param valueType JavaBean类型
+     * @param <T>       泛型
+     * @return JavaBean
+     */
     public static <T> T toObject(String content, Class<T> valueType) {
         if (content == null) {
             return null;
@@ -66,6 +79,14 @@ public class JacksonUtils {
         }
     }
 
+    /**
+     * json字符串转换为JavaBean,支持泛型
+     *
+     * @param content       json字符串
+     * @param typeReference 泛型
+     * @param <T>           泛型
+     * @return JavaBean
+     */
     public static <T> T toObject(String content, TypeReference<T> typeReference) {
         if (content == null) {
             return null;
@@ -77,6 +98,14 @@ public class JacksonUtils {
         }
     }
 
+    /**
+     * 将json反序列化成对象
+     *
+     * @param content  json字符串
+     * @param javaType JavaType
+     * @param <T>      泛型
+     * @return JavaBean
+     */
     public static <T> T toObject(String content, JavaType javaType) {
         if (content == null) {
             return null;
@@ -88,6 +117,52 @@ public class JacksonUtils {
         }
     }
 
+    /**
+     * 将json反序列化成对象
+     *
+     * @param in       InputStream 输入流
+     * @param javaType JavaType
+     * @param <T>      泛型
+     * @return JavaBean
+     */
+    public static <T> T toObject(InputStream in, JavaType javaType) {
+        if (in == null) {
+            return null;
+        }
+        try {
+            return objectMapper().readValue(in, javaType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 将json反序列化成对象
+     *
+     * @param content  json字符串
+     * @param javaType JavaType
+     * @param <T>      泛型
+     * @return JavaBean
+     */
+    public static <T> T toObject(byte[] content, JavaType javaType) {
+        if (content == null) {
+            return null;
+        }
+        try {
+            return objectMapper().readValue(content, javaType);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 读取集合
+     *
+     * @param content json字符串
+     * @param clazz   集合元素类型
+     * @param <T>     泛型
+     * @return 集合
+     */
     public static <T> List<T> toList(String content, Class<T> clazz) {
         if (content == null) {
             return null;
@@ -96,6 +171,16 @@ public class JacksonUtils {
         return toObject(content, javaType);
     }
 
+    /**
+     * 读取集合
+     *
+     * @param content    json字符串
+     * @param keyClass   key类型
+     * @param valueClass value类型
+     * @param <K>        key泛型
+     * @param <V>        value泛型
+     * @return 集合
+     */
     public static <K, V> Map<K, V> toMap(String content, Class<K> keyClass, Class<V> valueClass) {
         if (content == null) {
             return null;
@@ -104,6 +189,14 @@ public class JacksonUtils {
         return toObject(content, javaType);
     }
 
+    /**
+     * 读取集合
+     *
+     * @param content json字符串
+     * @param clazz   集合元素类型
+     * @param <T>     泛型
+     * @return 集合
+     */
     public static <T> Set<T> toSet(String content, Class<T> clazz) {
         if (content == null) {
             return null;
@@ -112,6 +205,14 @@ public class JacksonUtils {
         return toObject(content, javaType);
     }
 
+    /**
+     * 读取集合
+     *
+     * @param content json字符串
+     * @param clazz   集合元素类型
+     * @param <T>     泛型
+     * @return 集合
+     */
     public static <T> T[] toArray(String content, Class<T> clazz) {
         if (content == null) {
             return null;
@@ -120,6 +221,13 @@ public class JacksonUtils {
         return toObject(content, javaType);
     }
 
+    /**
+     * 读取集合
+     *
+     * @param content json字符串
+     * @param <T>     泛型
+     * @return 集合
+     */
     public static <T> T[] toArray(String content) {
         if (content == null) {
             return null;
@@ -128,6 +236,12 @@ public class JacksonUtils {
         });
     }
 
+    /**
+     * 将json字符串转换为JsonNode
+     *
+     * @param content json字符串
+     * @return JsonNode
+     */
     public static JsonNode toNode(String content) {
         if (content == null) {
             return null;
@@ -139,6 +253,12 @@ public class JacksonUtils {
         }
     }
 
+    /**
+     * 将json字符串转换为JsonNode
+     *
+     * @param jsonParser json解析器
+     * @return JsonNode
+     */
     public static JsonNode toNode(JsonParser jsonParser) {
         try {
             return objectMapper().readTree(jsonParser);
@@ -147,6 +267,12 @@ public class JacksonUtils {
         }
     }
 
+    /**
+     * 将json字符串转换为JsonNode
+     *
+     * @param content json字符串
+     * @return JsonNode
+     */
     public static JsonParser createParser(String content) {
         if (content == null) {
             return null;
