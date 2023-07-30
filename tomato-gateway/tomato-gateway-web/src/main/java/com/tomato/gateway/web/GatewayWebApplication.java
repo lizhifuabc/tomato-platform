d791cd5eec231e42d6aab66b17b91d24d05f81e5
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-30 21:15:15 +0800

    [新增] Reactor 3.5.3 前使用 MDC

--- tomato-gateway/tomato-gateway-web/src/main/java/com/tomato/gateway/web/GatewayWebApplication.java
+++ tomato-gateway/tomato-gateway-web/src/main/java/com/tomato/gateway/web/GatewayWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import reactor.core.publisher.Hooks;
 
 /**
  * 网关启动程序
@@ -28,5 +29,11 @@ public class GatewayWebApplication {
 
         Environment env = applicationContext.getEnvironment();
         log.info("环境信息{}",env);
+
+        // 为了在WebFlux中使用MDC，我们需要启用自动上下文传播
+        // https://github.com/micrometer-metrics/tracing/wiki/Spring-Cloud-Sleuth-3.1-Migration-Guide
+        // https://www.saoniuhuo.com/question/detail-2515079.html
+        // TODO Reactor 3.5.3 之后不需要这个了，但是目前还是需要的
+        Hooks.enableAutomaticContextPropagation();
     }
 }
