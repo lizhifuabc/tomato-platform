bacf3981829c1b409236940b3c63ca45c11f9fe4
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-30 17:10:24 +0800

    [修复] 去除无效代码

--- tomato-module/tomato-module-sys/tomato-module-sys-boot/src/main/java/com/tomato/sys/SysBootApplication.java
+++ tomato-module/tomato-module-sys/tomato-module-sys-boot/src/main/java/com/tomato/sys/SysBootApplication.java
@@ -1,11 +1,7 @@
 package com.tomato.sys;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
