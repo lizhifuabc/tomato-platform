dc2ca214802ee1a7dfe4046d081db02b827d4bec
拔土豆的程序员 <lizhifuabc@163.com>
2023-08-01 22:27:20 +0800

    [新增] 远程服务常量

---
+++ tomato-module/tomato-module-common/src/main/java/com/tomato/module/common/constants/ServiceNameConstants.java
@@ -0,0 +1,11 @@
+package com.tomato.module.common.constants;
+
+/**
+ * 远程服务常量
+ *
+ * @author lizhifu
+ * @since 2023/8/1
+ */
+public interface ServiceNameConstants {
+    String SERVICE_NAME_MERCHANT = "tomato-module-merchant";
+}
