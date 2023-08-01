76e282eb04aaa823b1d6fa4bb2c27832b0c1e90a
拔土豆的程序员 <lizhifuabc@163.com>
2023-08-01 13:09:20 +0800

    [优化] 优化代码结构

--- tomato-framework/tomato-idempotent-starter/src/main/java/com/tomato/idempotent/strategy/AbstractIdempotentStrategy.java
+++ tomato-framework/tomato-idempotent-starter/src/main/java/com/tomato/idempotent/strategy/AbstractIdempotentStrategy.java
@@ -1,5 +1,9 @@
 package com.tomato.idempotent.strategy;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.StandardCharsets;
+
 /**
  * 抽象幂等执行策略
  *
@@ -8,11 +12,17 @@ package com.tomato.idempotent.strategy;
  */
 public abstract class AbstractIdempotentStrategy implements IdempotentStrategy {
     /**
-     * 生成请求key
-     * @param sign 签名
+     * MD5加密
+     * @param input 签名
      * @return 请求key
      */
-    protected String generateRequestKey(String sign) {
-        return sign;
+    protected String generateRequestKey(String input) {
+        return Hashing.md5().hashString(input, StandardCharsets.UTF_8).toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "中国";
+        String s = Hashing.md5().hashString(input, StandardCharsets.UTF_8).toString();
+        System.out.println(s);
     }
 }
--- tomato-framework/tomato-idempotent-starter/src/main/java/com/tomato/idempotent/strategy/impl/RedisStrategyImpl.java
+++ tomato-framework/tomato-idempotent-starter/src/main/java/com/tomato/idempotent/strategy/impl/RedisStrategyImpl.java
@@ -2,13 +2,11 @@ package com.tomato.idempotent.strategy.impl;
 
 import com.tomato.common.exception.BusinessException;
 import com.tomato.idempotent.annotation.Idempotent;
-import com.tomato.idempotent.strategy.IdempotentStrategy;
+import com.tomato.idempotent.strategy.AbstractIdempotentStrategy;
 import org.aspectj.lang.JoinPoint;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Objects;
 
@@ -19,7 +17,7 @@ import java.util.Objects;
  * @since 2023/4/11
  */
 @Service
-public class RedisStrategyImpl implements IdempotentStrategy {
+public class RedisStrategyImpl extends AbstractIdempotentStrategy {
     private final StringRedisTemplate redisTemplate;
     public RedisStrategyImpl(StringRedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
@@ -29,41 +27,14 @@ public class RedisStrategyImpl implements IdempotentStrategy {
     public void execute(JoinPoint joinPoint, Idempotent idempotent) {
         String methodName = joinPoint.getSignature().toString();
         String argsStr = Arrays.toString(joinPoint.getArgs());
-        String redisKey = md5Encode(methodName + argsStr);
+        String redisKey = generateRequestKey(methodName + argsStr);
         Boolean aBoolean = redisTemplate.opsForValue().setIfAbsent(Objects.requireNonNull(redisKey), "", idempotent.timeout(), idempotent.timeUnit());
         if (Boolean.FALSE.equals(aBoolean)) {
             throw new BusinessException(idempotent.message());
         }
     }
-
     @Override
     public String name() {
         return "REDIS";
-    }
-
-    /**
-     * MD5加密 TODO 方法集成到工具类
-     *
-     * @param string 字符串
-     * @return String
-     */
-    public static String md5Encode(String string) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(string.getBytes());
-            byte[] digest = md.digest();
-            StringBuilder sb = new StringBuilder();
-            for (byte b : digest) {
-                // 0xff 为 16 进制
-                sb.append(String.format("%02x", b & 0xff));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            return null;
-        }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(md5Encode("中文"));
     }
 }
