2fbbd343d2d36236d4253f71f54dec5f7eab6459
拔土豆的程序员 <lizhifuabc@163.com>
2023-08-02 18:33:45 +0800

    [重构] 重构订单模块：订单创建事件监听

--- tomato-module/tomato-module-order/tomato-module-order-application/src/main/java/com/tomato/order/application/event/OrderCreateListener.java
+++ tomato-module/tomato-module-order/tomato-module-order-application/src/main/java/com/tomato/order/application/event/OrderCreateListener.java
@@ -1,5 +1,6 @@
 package com.tomato.order.application.event;
 
+import com.tomato.order.domain.repository.RabbitRepository;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +19,10 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderCreateListener implements ApplicationListener<OrderCreateEvent> {
+    private final RabbitRepository rabbitRepository;
     @Override
     public void onApplicationEvent(@NotNull OrderCreateEvent event) {
         log.info("OrderCreateListener 订单创建监听:{}",event.getOrderInfoEntity().getOrderNo());
+        rabbitRepository.createOrderTimeOut(event.getOrderInfoEntity());
     }
 }
