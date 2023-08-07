56752a4ef68459c478739d3e3204ecc4d835a3d9
拔土豆的程序员 <lizhifuabc@163.com>
2023-08-07 13:40:20 +0800

    [新增] nacos

--- doc/sql/tomato-database.sql
+++ doc/sql/tomato-database.sql
@@ -3,6 +3,10 @@
 * MySQL8.x版本
 */
 -- ----------------------------
+-- nacos数据库
+-- ----------------------------
+CREATE DATABASE IF NOT EXISTS `nacos` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
+-- ----------------------------
 -- 秒杀数据库
 -- ----------------------------
 CREATE DATABASE IF NOT EXISTS `tomato_seckill` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;
