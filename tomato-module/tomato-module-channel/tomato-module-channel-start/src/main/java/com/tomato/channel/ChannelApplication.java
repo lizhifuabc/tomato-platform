6abba660a5b493383ed9c34dc9285b0f68e44afb
拔土豆的程序员 <lizhifuabc@163.com>
2023-08-11 22:10:06 +0800

    [测试] spring boot admin  3

--- tomato-module/tomato-module-channel/tomato-module-channel-start/src/main/java/com/tomato/channel/ChannelApplication.java
+++ tomato-module/tomato-module-channel/tomato-module-channel-start/src/main/java/com/tomato/channel/ChannelApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -12,6 +13,7 @@ import org.springframework.core.env.Environment;
  * 启动类
  * @author lizhifu
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "渠道服务 API", version = "1.0-SNAPSHOT", description = "渠道服务 Information"),
 		servers = {@Server(url = "http://localhost:9991/")})
