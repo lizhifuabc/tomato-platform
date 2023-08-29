d00b6574c17ff635e7225788a83cd6618a8d1f1b
拔土豆的程序员 <lizhifuabc@163.com>
2023-08-29 10:24:48 +0800

    [新增] cloud core 提供一些基础配置

---
+++ tomato-framework/tomato-cloud/tomato-cloud-core/src/main/java/com/tomato/cloud/core/properties/BlackListProperties.java
@@ -0,0 +1,53 @@
+package com.tomato.cloud.core.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 黑名单配置,运行时刷新
+ *
+ * @author lizhifu
+ * @since 2023/8/29
+ */
+@Data
+@Component
+@RefreshScope
+@ConfigurationProperties("tomato.black-list")
+public class BlackListProperties {
+    /**
+     * 是否开启
+     */
+    private Boolean enable = true;
+    /**
+     * 黑名单IP
+     */
+    private List<String> ipList = Collections.emptyList();
+
+    /**
+     * 黑名单IP与服务
+     */
+    private List<BlackList> services = Collections.emptyList();
+    @Data
+    public static class BlackList {
+
+        /**
+         * IP
+         */
+        private String ip;
+
+        /**
+         * 服务名
+         */
+        private String name;
+
+        /**
+         * 路径 默认拦截所有：/**
+         */
+        private List<String> pathList = Collections.singletonList("/**");
+    }
+}
