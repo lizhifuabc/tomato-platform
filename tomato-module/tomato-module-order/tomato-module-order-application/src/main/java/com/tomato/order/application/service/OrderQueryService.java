f3a8552f6a5d6acc6ec523e87f31f6d8fd61ecad
拔土豆的程序员 <lizhifuabc@163.com>
2023-09-14 11:34:15 +0800

    [新增] 订单查询 hmac 校验

--- tomato-module/tomato-module-order/tomato-module-order-application/src/main/java/com/tomato/order/application/service/OrderQueryService.java
+++ tomato-module/tomato-module-order/tomato-module-order-application/src/main/java/com/tomato/order/application/service/OrderQueryService.java
@@ -1,12 +1,17 @@
 package com.tomato.order.application.service;
 
+import com.tomato.common.exception.BusinessException;
 import com.tomato.order.application.req.OrderQueryByMerchantReq;
 import com.tomato.order.application.req.OrderQueryByOrderNoReq;
 import com.tomato.order.application.req.OrderQueryResultResp;
+import com.tomato.order.application.util.HmacUtil;
 import com.tomato.order.domain.domain.entity.OrderInfoEntity;
 import com.tomato.order.domain.repository.OrderInfoRepository;
 import com.tomato.web.core.util.BeanUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 订单查询
@@ -18,18 +23,38 @@ import org.springframework.stereotype.Service;
 public class OrderQueryService {
 
 	private final OrderInfoRepository orderInfoRepository;
+	private final MerchantService merchantService;
 
-	public OrderQueryService(OrderInfoRepository orderInfoRepository) {
+	public OrderQueryService(OrderInfoRepository orderInfoRepository, MerchantService merchantService) {
 		this.orderInfoRepository = orderInfoRepository;
+		this.merchantService = merchantService;
 	}
 
 	public OrderQueryResultResp orderQueryByMerchant(OrderQueryByMerchantReq orderQueryByMerchantReq) {
+		// hmac 校验
+		String merchantKey = merchantService.merchantKey(orderQueryByMerchantReq.getMerchantNo());
+		Map<String,String> map = new HashMap<>(16);
+		map.put("merchantNo",orderQueryByMerchantReq.getMerchantNo());
+		map.put("merchantOrderNo",orderQueryByMerchantReq.getMerchantOrderNo());
+		String hmac = HmacUtil.hmac(map, merchantKey);
+		if (!hmac.equals(orderQueryByMerchantReq.getHmac())) {
+			throw new BusinessException("hmac校验失败");
+		}
 		OrderInfoEntity orderInfoEntity = orderInfoRepository.selectByMerchant(orderQueryByMerchantReq.getMerchantNo(),
 				orderQueryByMerchantReq.getMerchantOrderNo());
 		return convert(orderInfoEntity);
 	}
 
 	public OrderQueryResultResp orderQueryByOrderNo(OrderQueryByOrderNoReq orderQueryByOrderNoReq) {
+		// hmac 校验
+		String merchantKey = merchantService.merchantKey(orderQueryByOrderNoReq.getMerchantNo());
+		Map<String,String> map = new HashMap<>(16);
+		map.put("merchantNo",orderQueryByOrderNoReq.getMerchantNo());
+		map.put("orderNo",orderQueryByOrderNoReq.getOrderNo());
+		String hmac = HmacUtil.hmac(map, merchantKey);
+		if (!hmac.equals(orderQueryByOrderNoReq.getHmac())) {
+			throw new BusinessException("hmac校验失败");
+		}
 		OrderInfoEntity orderInfoEntity = orderInfoRepository.selectByOrderNo(orderQueryByOrderNoReq.getMerchantNo(),
 				orderQueryByOrderNoReq.getOrderNo());
 		return convert(orderInfoEntity);
