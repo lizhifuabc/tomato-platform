be47a5449b17ec7bea03fe8636539e92891c9e53
拔土豆的程序员 <lizhifuabc@163.com>
2024-03-14 12:44:37 +0800

    [优化] 分页查询请求

--- tomato-framework/tomato-common/src/main/java/com/tomato/common/domain/req/PageQueryReq.java
+++ tomato-framework/tomato-common/src/main/java/com/tomato/common/domain/req/PageQueryReq.java
@@ -1,70 +1,54 @@
 package com.tomato.common.domain.req;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.List;
+
 /**
  * 分页查询请求
  *
  * @author lizhifu
  * @since 2022/11/21
  */
+@Data
 public class PageQueryReq {
 
+	@Schema(description = "页码(不能为空)", example = "1")
+	@NotNull(message = "分页参数不能为空")
+	private Integer pageNum;
+
+	@Schema(description = "每页数量(不能为空)", example = "10")
+	@NotNull(message = "每页数量不能为空")
+	@Max(value = 200, message = "每页最大为200")
+	private Integer pageSize;
+
+	@Schema(description = "是否查询总条数")
+	protected Boolean searchCount;
+
+	@Schema(description = "排序字段集合")
+	@Size(max = 10, message = "排序字段最多10")
+	@Valid
+	private List<SortItem> sortItemList;
 	/**
-	 * 默认每页数量
+	 * 排序DTO类
 	 */
-	private static final int DEFAULT_PAGE_SIZE = 10;
+	@Data
+	public static class SortItem {
 
-	/**
-	 * 每页数量
-	 */
-	private int pageSize = DEFAULT_PAGE_SIZE;
+		@Schema(description = "true正序|false倒序")
+		@NotNull(message = "排序规则不能为空")
+		private Boolean isAsc;
 
-	/**
-	 * 页码
-	 */
-	private int pageIndex = 1;
-
-	/**
-	 * 是否查询总数
-	 */
-	private boolean needTotalCount = true;
-
-	public int getPageIndex() {
-		if (pageIndex < 1) {
-			return 1;
-		}
-		return pageIndex;
+		@Schema(description = "排序字段")
+		@NotBlank(message = "排序字段不能为空")
+		@Length(max = 30, message = "排序字段最多30")
+		private String column;
 	}
-
-	public PageQueryReq setPageIndex(int pageIndex) {
-		this.pageIndex = pageIndex;
-		return this;
-	}
-
-	public int getPageSize() {
-		if (pageSize < 1) {
-			pageSize = DEFAULT_PAGE_SIZE;
-		}
-		return pageSize;
-	}
-
-	public PageQueryReq setPageSize(int pageSize) {
-		if (pageSize < 1) {
-			pageSize = DEFAULT_PAGE_SIZE;
-		}
-		this.pageSize = pageSize;
-		return this;
-	}
-
-	public int getOffset() {
-		return (getPageIndex() - 1) * getPageSize();
-	}
-
-	public boolean isNeedTotalCount() {
-		return needTotalCount;
-	}
-
-	public void setNeedTotalCount(boolean needTotalCount) {
-		this.needTotalCount = needTotalCount;
-	}
-
 }
