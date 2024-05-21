6929c93a5e48015f2c9bfb35dc1416bda375f248
拔土豆的程序员 <lizhifuabc@163.com>
2024-05-21 13:43:35 +0800

    [优化] 数据源切换

--- tomato-framework/tomato-dynamic-db/src/main/java/com/tomato/dynamic/db/holder/DynamicDataSourceHolder.java
+++ tomato-framework/tomato-dynamic-db/src/main/java/com/tomato/dynamic/db/holder/DynamicDataSourceHolder.java
@@ -2,7 +2,7 @@ package com.tomato.dynamic.db.holder;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Deque;
+import java.util.LinkedList;
 import java.util.LinkedList;
 import java.util.Objects;
 
@@ -18,14 +18,14 @@ public class DynamicDataSourceHolder {
 	/**
 	 * 保存动态数据源名称
 	 */
-	private static final ThreadLocal<Deque<String>> DYNAMIC_DATASOURCE_KEY = new ThreadLocal<>();
+	private static final ThreadLocal<LinkedList<String>> DYNAMIC_DATASOURCE_KEY = new ThreadLocal<>();
 
 	/**
 	 * 设置/切换数据源，决定当前线程使用哪个数据源
 	 */
 	public static void setDynamicDataSourceKey(String key) {
 		log.info("数据源切换为：{}", key);
-		Deque<String> list = DYNAMIC_DATASOURCE_KEY.get();
+		LinkedList<String> list = DYNAMIC_DATASOURCE_KEY.get();
 		if (Objects.isNull(list)){
 			list = new LinkedList<>();
 		}
@@ -37,7 +37,7 @@ public class DynamicDataSourceHolder {
 	 * 获取动态数据源
 	 */
 	public static String getDynamicDataSourceKey() {
-		Deque<String> list = DYNAMIC_DATASOURCE_KEY.get();
+		LinkedList<String> list = DYNAMIC_DATASOURCE_KEY.get();
 		if (Objects.isNull(list) || list.isEmpty()) {
 			return null;
 		}
@@ -49,7 +49,7 @@ public class DynamicDataSourceHolder {
 	 */
 	public static void removeDynamicDataSourceKey() {
 		log.info("移除数据源：{}", DYNAMIC_DATASOURCE_KEY.get());
-		Deque<String> list = DYNAMIC_DATASOURCE_KEY.get();
+		LinkedList<String> list = DYNAMIC_DATASOURCE_KEY.get();
 		if (Objects.isNull(list) || list.isEmpty()) {
 			DYNAMIC_DATASOURCE_KEY.remove();
 			return;
