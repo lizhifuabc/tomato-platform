b249f6fc8c1459152f050fdc63b60b8dc86ba07e
lizhifu <lizhifuabc@163.com>
2024-07-15 16:13:52 +0800

    [新增] 添加序列号生成starter
    新增了一个序列号生成自动配置类和相关的pom配置，用于支持单据序列号的生成。在tomato-bom中升级了mybatis-mp版本至1.5.9-spring-boot3。

---
+++ tomato-framework/tomato-serial-number-starter/src/main/java/com/tomato/serial/number/SerialNumberAutoConfig.java
@@ -0,0 +1,14 @@
+package com.tomato.serial.number;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+/**
+ * 单据序列号 自动配置类
+ *
+ * @author lizhifu
+ * @since 2022/12/16
+ */
+@AutoConfiguration
+public class SerialNumberAutoConfig {
+
+}
